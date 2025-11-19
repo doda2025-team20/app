e63cf307186bb6985507ad873f516b9b1540a7ea
Hautelman Petre-Alexandru <p.a.hautelman-1@student.tudelft.nl>
2025-11-19 16:22:29 +0100

    add lib-version to pom.xml and implement endpoint that returns the version

--- src/main/java/frontend/ctrl/HelloWorldController.java
+++ src/main/java/frontend/ctrl/HelloWorldController.java
@@ -3,6 +3,7 @@ package frontend.ctrl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sms.VersionUtil;
 
 @Controller
 public class HelloWorldController {
@@ -11,5 +12,11 @@ public class HelloWorldController {
     @ResponseBody
     public String index() {
         return "Hello World!";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String about() {
+        return "Version: " + VersionUtil.getVersion();
     }
 }
